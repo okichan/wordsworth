8f2d1483e98a1b27c0c171c665322cc09a9d082b
Tomomi <madeinwatashi@gmail.com>
2017-10-30 15:52:48 +1100

    Add favicon

---
+++ app/controllers/landing_controller.rb
@@ -0,0 +1,6 @@
+class LandingController < ApplicationController
+    def index
+      @user = current_user
+    end
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :profiles
   resources :questions
   devise_for :users
-  root to: "questions#index"
+  root 'landing#index'
+  
 end
