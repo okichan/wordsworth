df587dc793cae7c5c879b82dc74cff8aa774bc5d
Tomomi <madeinwatashi@gmail.com>
2017-10-31 03:49:51 +1100

    Add responsive

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,31 @@ class Question < ApplicationRecord
   belongs_to :user
   has_many :answers
   validates :title, presence: true
+
+   LANG_OPTIONS = [ "Arabic", 
+   "Cantonese", 
+   "Danish", 
+   "Dutch", 
+   "English", 
+   "French", 
+   "German", 
+   "Greek", 
+   "Hindi", 
+   "Indonesian", 
+   "Italian", 
+   "Japanese", 
+   "Korean", 
+   "Mandarin", 
+   "Norwegian", 
+   "Polish", 
+   "Portuguese", 
+   "Russian", 
+   "Spanish", 
+   "Swedish", 
+   "Thai", 
+   "Turkish", 
+   "Vietnamese"]
+  
+  
+  
 end
